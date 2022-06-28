95066ef1b233c984104bf49613c3d17d082c29e3
Nabeel-Chhipa <nabeelchipa@gmail.com>
2022-06-28 15:24:33 +0500

    add useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import UseState from './Hooks/useStateHook/UseState'
+import UseEffect from './Hooks/useEffectHook/UseEffect';
 
 function App() {
   return (
     <div className="App">
-      <UseState />
+      {/* <UseState /> */}
+      <UseEffect />
     </div>
   );
 }
--- src/Hooks/useStateHook/UseState.js
+++ src/Hooks/useStateHook/UseState.js
@@ -3,14 +3,19 @@ import React, { useState } from 'react'
 const UseState = () => {
 
   const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState('Blue')
 
-  const increment = () => setCount(preValue => preValue+1)
+  const increment = () => {
+    setCount(preValue => preValue+1)
+    setTheme('Black')
+  }
   const decrement = () => setCount(preValue => preValue-1)
 
   return (
     <>
       <button onClick={decrement}>-</button>
       <span>{count}</span>
+      <span>{theme}</span>
       <button onClick={increment}>+</button>
     </>
   )
